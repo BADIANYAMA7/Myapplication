4a8d6d24a22dcf0000f95794ed0e52d2e481e747
hp <ben.blessing2006@gmail.com>
2025-03-25 14:18:05 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/st10407775/Myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package za.co.varsitycollege.st10407775.Myapplication
+
+import
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        }
+    }
+}
